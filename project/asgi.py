3d281af21885060069ac9b61cc954fd212c730d8
Leonard <limlleonard@gmail.com>
2025-04-22 17:39:33 +0200

    rename project2

--- project/asgi.py
+++ project/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "back.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 # Load Django application first to initialize settings
 django_asgi_app = get_asgi_application()
